77fdc04bbc88c198adee6ac0aa2e75548b6de8e3
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-06-02 23:07:45 +0530

    Remove TreeNode structure from BFS and DFS cycle detection files; clean up undirected cycle detection implementations

--- Undirected Cycle Detection copy/BFS_Cycle.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
