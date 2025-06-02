0725b3334e59945d32808cf29b06836d0eca7a07
Shardendu Mishra <shardendumishrabrave@gmail.com>
2025-06-03 01:22:44 +0530

    Refactor BFS and DFS cycle detection implementations; format includes and improve code structure

--- Undirected Cycle Detection/BFS_Cycle.cpp
+++ Undirected Cycle Detection/BFS_Cycle.cpp
@@ -1,2 +1,44 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+bool isCyclicBFS(vector<vector<int>>& edges,int numberNodes,int numberEdges){
+    vector<vector<int>> adj(numberNodes+1);
+
+    
+    for(int i=0;i<numberEdges;i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u); 
+    }
+
+    queue<int> q;
+    vector<int> parent(numberNodes+1, -1);
+    vector<bool> visited(numberNodes+1, false);
+
+    for(int i = 1;i < numberNodes + 1;i += 1){
+        if(visited[i]){
+            continue;
+        }
+        
+        q.push(i);
+        parent[i] = -1;
+        visited[i] = true;
+
+        while(!q.empty()){
+            int currNode = q.front();
+            q.pop();
+
+            for(int neighbour:adj[currNode]){
+                if(!visited[neighbour]){
+                    q.push(neighbour);
+                    visited[neighbour] = true;
+                    parent[neighbour] = currNode;
+                }else if(parent[currNode] != neighbour){
+                    return true;
+                }
+            }
+        }
+    }
+}
--- Undirected Cycle Detection/DFS_Cycle.cpp
+++ Undirected Cycle Detection/DFS_Cycle.cpp
@@ -1,5 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
+bool dfs(vector<vector<int>>& adj, vector<bool>& visited, vector<int>& parent,int currNode){
+    visited[currNode] = true;
 
+    for(int neightbour:adj[currNode]){
+        if(!visited[neightbour]){
+            parent[neightbour] = currNode;
+            if(dfs(adj, visited, parent, neightbour)){
+                return true;
+            }
+        }else if(parent[currNode] != neightbour){
+            return true;
+        }
+    }
+}
 
+bool isCyclicDFS(vector<vector<int>>& edges,int numberNodes,int numberEdges){
+    vector<vector<int>> adj(numberNodes+1);
+
+    
+    for(int i=0;i<numberEdges;i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u); 
+    }
+
+    vector<int> parent(numberNodes+1, -1);
+    vector<bool> visited(numberNodes+1, false);
+
+    for(int i = 1;i < numberNodes + 1;i += 1){
+        if(visited[i]) continue;
+
+        if(dfs(adj,visited,parent,i)){
+            return true;
+        }
+    }
+    return false;
+}
